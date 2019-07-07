7a1a6e2967f6d60e1a385248c5eb4bdbb96579e4
Oscar Maritim <oscarmaritim3@gmail.com>
2019-07-08 02:10:23 +0300

    added styles

---
+++ src/app/quotes.ts
@@ -0,0 +1,8 @@
+export class Quotes {
+
+constructor(public author: string, public say: string, public submitter: string, public posting: Date,public upvotes: number,
+    public downvotes: number){
+	
+}
+
+}
---
+++ src/app/quotes/quotes.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { Quotes } from '../quotes';
+
+@Component({
+  selector: 'app-quotes',
+  templateUrl: './quotes.component.html',
+  styleUrls: ['./quotes.component.css']
+})
+export class QuotesComponent implements OnInit {
+
+
+  quotesModel: Quotes;
+  showNew: Boolean = false;
+  submitType: string = 'Save';
+ 
+
+  onNew() {
+    this.quotesModel = new Quotes("", "", "",new Date(), 0, 0);
+
+    this.submitType = 'Save';
+
+    this.showNew = true;
+  }
+   
+
+
+  onSave() {
+    if (this.submitType === 'Save') {
+
+      this.quotes.push(this.quotesModel);
+    }
+
+    this.showNew = false;
+  }
+
+
+  onCancel() {
+
+    this.showNew = false;
+  }
+
+quotes =  [
+new Quotes ( 'Isaac Newton', 'Gravity is a force that pulls things toward the Earth, an it affects all the features on Earth', 'Oscar',new Date(2019),0,0),
+new Quotes ( 'Bob Colymore', 'The road to success is hard only the strong hearted make it','oscar',new Date(),0,0),
+
+];
+delete(i) {
+    this.quotes.splice(i, 1);
+      }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
