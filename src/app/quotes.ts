1cf03d4d78437eb2304d1e5b9dae772a285f88aa
Oscar Maritim <oscarmaritim3@gmail.com>
2019-07-08 10:37:39 +0300

    adding the quotes component

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,6 +1,6 @@
 export class Quotes {
 
-constructor(public author: string, public say: string, public submitter: string, public posting: Date,public upvotes: number,
+constructor(public author: string, public say: string, public submitter: string, public completeDate: Date,public upvotes: number,
     public downvotes: number){
 	
 }
